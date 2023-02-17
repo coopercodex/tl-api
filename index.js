8c3520f1b8259bc6b11a61f9303de76a7a96ab56
Derek Cooper <derekcooper20@gmail.com>
2023-02-17 16:20:33 -0700

    Fixes cors

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const tickets = require('./api/tickets')
 const app = express()
+const cors = require('cors')
 
 const PORT = process.env.PORT || 4000
 
 app.use("/api/tickets", tickets)
+app.use(cors())
 
 app.listen(PORT, () => console.log(`Server is running on ${PORT}`))
